8681a04df4e1ec9d90d4502f15bb8ce2abdc0a9a
Kia <2592730+superbaud@users.noreply.github.com>
2014-06-30 06:02:20 -0700

    wtf compiler

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -981,7 +981,7 @@ public: // for now...
       /* counter */
       uint64_t counter = 0;
       /* key */
-      uint8_t aes_key [] = "YELLOW SUBMARINE";
+      uint8_t aes_key [] = "YELLO";
 
       /* output status -- it's 0 if we need to output the high half of the
          block 1 if we need to output the low half (and then increment the
