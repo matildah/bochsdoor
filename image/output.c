00d4c54866632cca38333b69c386733f73f4cecd
Kia <2592730+superbaud@users.noreply.github.com>
2014-06-30 13:28:06 -0700

    do two at once

--- image/output.c
+++ image/output.c
@@ -1,13 +1,12 @@
 #include <stdint.h>
 
 int main() {
-    volatile uint64_t c;
+    volatile uint64_t c,d;
 
-    asm volatile("rdrand  %0" : "=r"(c));
-
-
-    
+    asm volatile("rdrand  %0\n\t"
+                 "rdrand  %1": "=r"(c), "=r"(d));
     printf("%p\n", c);
+    printf("%p\n", d);
 }
 
 
