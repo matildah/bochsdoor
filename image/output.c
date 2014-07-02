2f2baa5c4baa80b43c6af84333eb3a6037e0fb2e
Kia <2592730+superbaud@users.noreply.github.com>
2014-07-01 20:30:19 -0700

    %016lX

--- image/output.c
+++ image/output.c
@@ -2,11 +2,12 @@
 
 int main() {
     volatile uint64_t c,d;
-
+    c = 0xaaabadbadbadbeef;
+    d = 0xbeefbeefbeefbeef;
     asm volatile("rdrand  %0\n\t"
                  "rdrand  %1": "=r"(c), "=r"(d));
-    printf("%p\n", c);
-    printf("%p\n", d);
+    printf("%016lX", c);
+    printf("%016lX\n", d);
 }
 
 
