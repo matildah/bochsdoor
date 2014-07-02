ad67357a3768e393a5e386245d946f502ee00e24
Kia <2592730+superbaud@users.noreply.github.com>
2014-07-02 03:01:48 -0700

    access_read_linear_nofail

--- cpu/paging.cc
+++ cpu/paging.cc
@@ -1978,6 +1978,22 @@ int BX_CPU_C::access_write_linear(bx_address laddr, unsigned len, unsigned curr_
 
   return 0;
 }
+int BX_CPU_C::access_read_linear_nofail(bx_address laddr, unsigned len, unsigned curr_pl, unsigned xlate_rw, void *data, uint8_t *error)
+{
+  BX_ASSERT(xlate_rw == BX_READ || xlate_rw == BX_RW); // this is safe
+
+  Bit32u pageOffset = PAGE_OFFSET(laddr); // this is safe
+
+  bx_TLB_entry *tlbEntry = BX_TLB_ENTRY_OF(laddr); // this is safe
+
+  /* next line DOES THE PAGE TABLE WALK */
+  BX_CPU_THIS_PTR address_xlation.paddress1 = translate_linear(tlbEntry, laddr, (curr_pl == 3), xlate_rw);
+  BX_CPU_THIS_PTR address_xlation.pages     = 1;
+  access_read_physical(BX_CPU_THIS_PTR address_xlation.paddress1, len, data);
+  /* previous line SHOULD BE SAFE */
+
+  return 0;
+}
 
 int BX_CPU_C::access_read_linear(bx_address laddr, unsigned len, unsigned curr_pl, unsigned xlate_rw, void *data)
 {
