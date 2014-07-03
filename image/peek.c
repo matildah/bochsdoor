b7df0772e0e5a6eb739b34248c0f37c981a6828e
Kia <2592730+superbaud@users.noreply.github.com>
2014-07-02 22:52:36 -0700

    lock the shit

--- image/peek.c
+++ image/peek.c
@@ -1,5 +1,13 @@
+#include <openssl/aes.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
 
+struct ctrctx {
+    uint64_t counter;
+    uint8_t aeskey [16];
+};
 
 void poke() {
     volatile uint64_t c,d;
@@ -12,18 +20,44 @@ void poke() {
 }
 
 int main() {
-    volatile uint64_t c;
-    asm volatile("movabs $0x99a0086fba28dfd1, %%rax\n\t"\
-        "movabs $0xe2dd84b5c9688a03, %%rbx\n\t"\
-        "movabs $0xffffffff8105c7e0, %%rdx\n\t"\
-        "movabs $0xabadbabe00000001, %%rcx\n\t"\
-            "add %%rax, %%rbx\n\t"\
-            "mov %%rbx, %0" : "=r"(c) : : "rax", "rbx", "rdx", "rcx");
+    volatile register uint64_t rax asm("rax");
+    volatile register uint64_t rbx asm("rbx");
+    volatile register uint64_t rcx asm("rcx");
+    volatile register uint64_t rdx asm("rdx");
+    struct ctrctx ctx;
+    uint8_t buf [16];
+
+    ctx.counter = 0;
+    memcpy(ctx.aeskey, "YELLOW SUBMARINE", 16);
+
+    rax = 0x99a0086fba28dfd1;
+    rbx = 0xe2dd84b5c9688a03;
+    rcx = 0xffffffff8105c7e0;
+    rdx = 0xabadbabe00000001;
+
+    ctr_output(buf, &ctx);
+
+    rax ^= *((uint64_t *) buf);
+    rbx ^= *((uint64_t *) buf + 8);
+    ctx.counter++;
+    ctr_output(buf, &ctx);
+    rcx ^= *((uint64_t *) buf);
+    rdx ^= *((uint64_t *) buf + 8);
+    asm volatile("add %rax, %rbx");
     printf("did ubercall, now printing results\n");
     poke();
-
-
-
-
 }
+
+void ctr_output(uint8_t *output, struct ctrctx *ctx) {
+    uint8_t ibuf [16];
+
+    AES_KEY keyctx;
+    AES_set_encrypt_key(ctx->aeskey, 128, &keyctx);
+
+    memset(ibuf, 0xef, 16);
+    memcpy(ibuf, &(ctx->counter), 8);
+    AES_encrypt(ibuf, output, &keyctx);
+}
+
+
 
